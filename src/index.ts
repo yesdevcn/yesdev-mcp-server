c8eab49d917822eb79716539ce3f0b80e02fc3d6
dogstar <huangchanzong@yesapi.cn>
2025-06-23 17:59:36 +0800

    添加运行方式指定

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { registerTaskTools } from './mcp/tools/task.js';
